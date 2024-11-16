222c9d31e12c3e459e8c5dffb2a8fa1e3412d404
Lev1reG <derentanaphan@mail.ugm.ac.id>
2024-11-16 14:12:38 +0700

    feat: route group for dashboard

---
+++ app/(dashboard)/dashboard/page.js
@@ -0,0 +1,9 @@
+export default function Dashboard() {
+  return (
+    <div className="w-full h-full flex justify-center items-center">
+      <h1 className="font-bold text-black text-5xl">
+        Dashboard
+      </h1>
+    </div>
+  );
+}
