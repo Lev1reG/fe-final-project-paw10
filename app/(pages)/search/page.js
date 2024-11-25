5526f120c82559e564d4330052dff25c2f163130
IniDani <msaarafli007@gmail.com>
2024-11-26 01:49:37 +0700

    feat: Add dummy data for testing search result page

--- app/(pages)/search/page.js
+++ app/(pages)/search/page.js
@@ -13,9 +13,19 @@ export default function BookPage() {
     onlyAvailable: false, // Default value
   });
 
-  return (
+  // Sample data for books (replace with actual data)
+  const dummyBooks = [
+    { title: 'Book Title 1', author: 'Author 1', stock: 10, imageUrl: '/images/book1.jpg' },
+    { title: 'Book Title 2', author: 'Author 2', stock: 5, imageUrl: '/images/book2.jpg' },
+    { title: 'Book Title 3', author: 'Author 3', stock: 3, imageUrl: '/images/book3.jpg' },
+    { title: 'Book Title 4', author: 'Author 4', stock: 8, imageUrl: '/images/book4.jpg' },
+    { title: 'Book Title 5', author: 'Author 5', stock: 6, imageUrl: '/images/book5.jpg' },
+    { title: 'Book Title 6', author: 'Author 6', stock: 7, imageUrl: '/images/book6.jpg' },
+  ];
+
+  return (  
     <div className={twMerge("w-full min-h-screen bg-[#F9F6EE] p-8")}> {/* Set the background color and padding */}
-      <div className="flex flex-row gap-8">
+      <div className="flex flex-row gap-8 justify-between">
         {/* Left side: Filters */}
         <div className="w-full sm:w-1/4 mb-8">
           <FilterSidebar filters={filters} setFilters={setFilters} />
@@ -27,19 +37,17 @@ export default function BookPage() {
 
           {/* Display Book Cards */}
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-            <BookCard
-              title="Book Title 1"
-              author="Author 1"
-              stock={10}
-              imageUrl="/images/book1.jpg"
-            />
-            <BookCard
-              title="Book Title 2"
-              author="Author 2"
-              stock={5}
-              imageUrl="/images/book2.jpg"
-            />
-            {/* Add more BookCard components here */}
+            {dummyBooks.map((book, index) => (
+              <div className="w-[260px]">
+              <BookCard
+                key={index}
+                title={book.title}
+                author={book.author}
+                stock={book.stock}
+                imageUrl={book.imageUrl}
+              />
+              </div>
+            ))}
           </div>
         </div>
       </div>
