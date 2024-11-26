1f2782037d2024b53444a0095e29b839a6a434ee
IniDani <msaarafli007@gmail.com>
2024-11-26 21:27:20 +0700

    fix: Stundent number in footer

--- components/all-pages/footer.jsx
+++ components/all-pages/footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
                     Deren Tanaphan (22/502361/TK/54976) <br />
                     Aisa Selvira Q.A (22/498561/TK/54690) <br />
                     Moh. Nazril Ilham (22/493142/TK/54000) <br />
-                    Muhammad Rafli Ramadani (22/497378/TK/54571) <br />
+                    Muhammad Rafli Ramadani (22/497787/TK/54571) <br />
                     Satama Safika (22/492880/TK/53955)
                 </div>
             </div>
